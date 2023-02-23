9e6f358a5683a9768e87a70e19cb08d3c112b2f5
Guillaume Blanc <guillaume.blanc@ubisoft.com>
2023-02-23 13:55:02 +0100

    Build standardized data files

--- update_std.py
+++ update_std.py
@@ -3,6 +3,7 @@ import sys
 import logging
 import argparse
 import glob
+import json
 import pandas as pd
 from pathlib import Path
 from datetime import datetime, timezone
@@ -11,13 +12,33 @@ from extern.fusnic import fusnic
 import src.std_utils as std_utils
 
 
-def update(previous: Path, latest: Path, updated: Path):
+def rename(data: pd.DataFrame, rename_conf: Path):
+    if not rename_conf.is_file():
+        logging.fatal('No renaming configuration provided')
+        return data
+
+    try:
+        with open(rename_conf) as f:
+            rdict = json.load(f)
+    except:
+        logging.fatal(
+            'Failed to decode json renaming configuration: %' % rename_conf)
+        raise
+    else:
+        subset = data[[c for c in data.columns if c in rdict.keys()]]
+        return subset.rename(columns=rdict)
+
+
+def update(previous: Path, latest: Path, updated: Path, rename_conf: Path):
     '''
     Update (ie add new rows) all files based on their name.
+    Latest column names can be changed with "rename" configuration dict
     '''
     for lfile in latest.glob('*.csv'):
         try:
-            udata = ldata = std_utils.from_csv(lfile)
+            ldata = std_utils.from_csv(lfile)
+            udata = ldata = rename(ldata, rename_conf)
+
         except pd.errors.EmptyDataError:
             logging.info(
                 'Empty data for latest file: %s. Skipping update.' % lfile)
@@ -47,10 +68,13 @@ if __name__ == '__main__':
                         help='Latest data')
     parser.add_argument('-u', '--updated', required=True,
                         help='Updated data')
+    parser.add_argument('-r', '--rename', default='',
+                        help='Column renaming configuration json file')
     parser.add_argument('-ll', '--loglevel', default='info',
                         help='Logging level. Example --loglevel debug, default=info')
     args = parser.parse_args()
 
     logging.basicConfig(level=args.loglevel.upper())
 
-    update(Path(args.previous), Path(args.latest), Path(args.updated))
+    update(Path(args.previous), Path(args.latest),
+           Path(args.updated), Path(args.rename))
