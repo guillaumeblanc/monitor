fac0ccb5f661c04eddae2cd486c6dceb6f188fd6
Guillaume Blanc <guillaume.blanc@ubisoft.com>
2023-07-17 13:28:29 +0200

    Exposes collection date

--- etl_collect_fus.py
+++ etl_collect_fus.py
@@ -13,11 +13,10 @@ import src.std_utils as std_utils
 import src.hfs_utils as hfs_utils
 
 
-def collect(output: Path, username: str, password: str, tz: pytz.timezone, mock: bool):
+def collect(output: Path, username: str, password: str, date: datetime, tz: pytz.timezone, mock: bool):
     '''
     Query all plants data from Fusion Solar Northbound interface.
     '''
-    now = datetime.now(tz=timezone.utc)
 
     try:
         '''session=MockSession() if mock else'''
@@ -29,7 +28,7 @@ def collect(output: Path, username: str, password: str, tz: pytz.timezone, mock:
             [logging.info(' - ' + plant['plantName'] +
                           ' (' + plant['plantCode'] + ')') for plant in plants]
             std_utils.to_csv(pd.DataFrame(plants), output /
-                             std_utils.format_filename('plants', now))
+                             std_utils.format_filename('plants', date))
 
             # Extract the list of plants code
             plants_code = [plant['plantCode'] for plant in plants]
@@ -38,53 +37,46 @@ def collect(output: Path, username: str, password: str, tz: pytz.timezone, mock:
             logging.info('Querying realtime data.')
             realtime = client.get_plant_realtime_data(plants_code)
             for entry in realtime:  # Adds time information
-                entry.update({'collectTime': client.to_timestamp(now)})
+                entry.update({'collectTime': client.to_timestamp(date)})
             logging.info('- Found ' + str(len(realtime)) + ' realtime data')
             std_utils.to_csv(hfs_utils.flatten(realtime, tz), output /
-                             std_utils.format_filename('realtime', now))
+                             std_utils.format_filename('realtime', date))
 
             # Hourly data
             logging.info('Querying hourly data.')
-            hourly = client.get_plant_hourly_data(plants_code, now)
+            hourly = client.get_plant_hourly_data(plants_code, date)
             logging.info('- Found ' + str(len(hourly)) + ' hourly data')
             std_utils.to_csv(hfs_utils.flatten(hourly, tz), output /
-                             std_utils.format_filename('hourly', now))
+                             std_utils.format_filename('hourly', date))
 
             # Daily data
             logging.info('Querying daily data.')
-            daily = client.get_plant_daily_data(plants_code, now)
+            daily = client.get_plant_daily_data(plants_code, date)
             logging.info('- Found ' + str(len(daily)) + ' daily data')
             std_utils.to_csv(hfs_utils.flatten(daily, tz), output /
-                             std_utils.format_filename('daily', now))
+                             std_utils.format_filename('daily', date))
 
             # Monthly data
             logging.info('Querying monthly data.')
-            monthly = client.get_plant_monthly_data(plants_code, now)
+            monthly = client.get_plant_monthly_data(plants_code, date)
             logging.info('- Found ' + str(len(monthly)) + ' monthly data')
             std_utils.to_csv(hfs_utils.flatten(monthly, tz), output /
-                             std_utils.format_filename('monthly', now))
+                             std_utils.format_filename('monthly', date))
 
             # Yearly data
             logging.info('Querying yearly data.')
-            yearly = client.get_plant_yearly_data(plants_code, now)
+            yearly = client.get_plant_yearly_data(plants_code, date)
             logging.info('- Found ' + str(len(yearly)) + ' yearly data')
             std_utils.to_csv(hfs_utils.flatten(yearly, tz), output /
-                             std_utils.format_filename('yearly', now))
-            
-            # Alarms data
-            logging.info('Querying alarms data.')
-            alarms = client.get_alarms_list(plants_code, datetime.datetime(2000, 1, 1), now)
-            logging.info('- Found ' + str(len(alarms)) + ' alarms')
-            std_utils.to_csv(fus_utils.flatten(alarms), output /
-                             std_utils.format_filename('alarms', now))
+                             std_utils.format_filename('yearly', date))
 
             # Alarms data
             logging.info('Querying alarms data.')
             alarms = client.get_alarms_list(
-                plants_code, datetime(2000, 1, 1), now)
+                plants_code, datetime(2000, 1, 1), date)
             logging.info('- Found ' + str(len(alarms)) + ' alarms')
             std_utils.to_csv(hfs_utils.flatten(alarms, tz), output /
-                             std_utils.format_filename('alarms', now))
+                             std_utils.format_filename('alarms', date))
 
     except pyhfs.LoginFailed:
         sys.exit(
@@ -103,6 +95,8 @@ if __name__ == '__main__':
                         help='FusionSolar Northbound interface password')
     parser.add_argument('-o', '--output', default='out',
                         help='Output directory. Example --output your_path, default=out')
+    parser.add_argument('-d', '--date', default='',
+                        help='Collection date with format d/m/Y. Example --date 07/07/2023, default to now')
     parser.add_argument('-m', '--mock', default=False, action=argparse.BooleanOptionalAction,
                         help='Mock fusion solar data')
     parser.add_argument('-t', '--timezone',
@@ -113,5 +107,8 @@ if __name__ == '__main__':
 
     logging.basicConfig(level=args.loglevel.upper())
 
+    date = datetime.strptime(
+        args.date, '%d/%m/%Y') if args.date else datetime.now(tz=timezone.utc)
+
     collect(output=Path(args.output), username=args.username,
-            password=args.password, tz=pytz.timezone(args.timezone), mock=args.mock)
+            password=args.password, date=date, tz=pytz.timezone(args.timezone), mock=args.mock)
